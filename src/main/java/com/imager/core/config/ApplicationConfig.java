18caeddcfb7d32fa13b9f2cd3680ea7a46552dc4
sai krishna <saikrishna1908@gmail.com>
2021-07-18 21:04:46 +0530

    Edit Dockerfile

--- src/main/java/com/imager/core/config/ApplicationConfig.java
+++ src/main/java/com/imager/core/config/ApplicationConfig.java
@@ -1,9 +1,5 @@
 package com.imager.core.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
